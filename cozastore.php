3978e808ba745e1fe11d2d55e1ffe084d5b9c7b2
obochikennethodinaka <kennethobochi33@gmail.com>
2025-12-05 22:59:53 -0800

    Add files via upload

---
+++ cozastore.php
@@ -0,0 +1,312 @@
+<?php
+
+$document = [
+     ["id"=> 0, "images1"=>"statue-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"],    
+      ["id"=> 1, "images1"=>"books-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+       ["id"=> 2, "images1"=>"clock-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+        ["id"=> 3, "images1"=>"grayscale-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+         ["id"=> 4, "images1"=>"lamp-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+          ["id"=> 5, "images1"=>"phone-and-keyboard-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+           ["id"=> 6, "images1"=>"red-and-blue-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+            ["id"=> 7, "images1"=>"standard-2400.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+             ["id"=> 8, "images1"=>"tulips-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+              ["id"=> 9, "images1"=>"white-lamp-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+               ["id"=> 10, "images1"=> "woodcraft-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+                ["id"=> 11, "images1"=>"beetle-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+                  ["id"=> 0, "images1"=>"statue-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"],    
+                  ["id"=> 1, "images1"=>"books-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+                  ["id"=> 2, "images1"=>"clock-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+                  ["id"=> 3, "images1"=>"grayscale-1200.jpg", "title"=>"DESIGN BY: NARUTO UZUMAKI", "header"=>"Just A Normal Simple Blog Post", "content"=>"Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam iusto nulla suscipit corrupti pariatur, ut labore dolorum sapiente nihil illum, quibusdam eius odit saepe id quos voluptates sed earum sequi!", "button"=>"READ MORE"], 
+];
+
+$folder = [
+    ["content1"=>"Women", "content2"=>"New Trend", "image"=>"grayscale-1200.jpg"],
+     ["content1"=>"Men", "content2"=>"New Trend", "image"=>"grayscale-1200.jpg"],
+      ["content1"=>"Accessories", "content2"=>"New Trend", "image"=>"grayscale-1200.jpg"],
+];
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../php_class/bootstrap5/bootstrap.min.css">
+    <link rel="stylesheet" href="../php_class/font-awesome-4.7.0/css/font-awesome.min.css">
+    <script src="../php_class/bootstrap5/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="../php_class/css/cozastore.css">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<a href="#" class="anchor"><i class="fa fa-arrow-up" style="color: black;"></i></a>
+
+  <!-- top nav -->
+     <div class="container-fluid bg-black lg-5  text-light w-100 d-flex align-items-center justify-content-center" style="height: 60px; overflow: hidden;">
+    <div class="container-fluid w-90 ms-3" style="font-size: 12px;">
+        Free Shipping For Standard Order Over $600
+    </div>
+    <span class="container-fluid d-flex align-items-center justify-content-center mt-3" style="font-size: 12px;">
+        <p class="me-4"> HELP AND FAQ</p>
+         <p class="me-4">My Accounts</p>
+          <p class="me-4">USD</p>
+           <p class="me-4">NAIRA</p>
+    </span>
+</div>
+
+
+<!-- <img
+                            src="../images/image-removebg-preview (18).png" height="20" width="20" class="mb-2" alt=""> -->
+
+<!-- navigation bar -->
+         <div class="container-fluid " style="background-color:lightgray;">
+            <div class="navbar navbar-expand-md bg-sm-primary">
+                <div class="navbar-brand">
+                    <p class=" text-dark fw-bold f2 px-5 text-uppercase">
+                        COZASTORE</p>
+                </div>
+                <button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#demo">
+                    <span class="navbar-toggler-icon bg-light rounded"></span>
+                </button>
+                <div class=" collapse navbar-collapse" id="demo">
+                    <ul class="navbar-nav d-flex justify-content-end  w-100">
+                        <li class="nav-item text-center fw-bold mb-3">
+                            <a href="" class="nav-link text-danger">HOME</a>
+                        </li>
+                        <li class="nav-item text-center fw-lighter">
+                            <a href="" class="nav-link text-darkt">SHOP</a>
+                        </li>
+
+                        <li class="nav-item text-center fw-lighter">
+                            <a href="" class="nav-link text-dark">FEATURES</a>
+                        </li>
+                        <li class="nav-item text-center fw-lighter">
+                            <a href="" class="nav-link text-dark">BLOG</a>
+                        </li>
+                        <li class="nav-item text-center fw-lighter">
+                            <a href="" class="nav-link text-dark">ABOUT</a>
+                        </li>
+                          <li class="nav-item text-center fw-lighter">
+                            <a href="" class="nav-link text-dark">CONTACT</a>
+                        </li>
+                        
+
+                </div>
+                 <div class="container-fluid d-flex justify-content-end ">
+                    <span class="d-flex justify-content-end me-3">
+                        <img src="../php_class/images1/image-removebg-preview (17).png" style="height: 30px; width: 30px;" alt=""><span
+                                class="badge position-absolute top-0-start-100 translate-middle badge rounded-pill bg-danger">20</span>
+                            <span class="visually-hidden">unread message</span></a>
+                    </span>
+                     <span class="d-flex justify-content-end me-3">
+                        <img src="../php_class/images1/image-removebg-preview (20).png" style="height: 30px; width: 30px;" alt=""><span
+                                class="badge position-absolute top-0-start-100 translate-middle badge rounded-pill bg-danger">20</span>
+                            <span class="visually-hidden">unread message</span></a>
+                    </span>
+                     <span class="d-flex justify-content-end me-3">
+                        <img src="../php_class/images1/image-removebg-preview (18).png" style="height: 30px; width: 30px;" alt=""><span
+                                class="badge position-absolute top-0-start-100 translate-middle badge rounded-pill bg-danger">20</span>
+                            <span class="visually-hidden">unread message</span></a>
+                    </span>
+                    
+                    </div>
+            </div>
+
+
+            <!-- content -->
+            <div class="row justify-content-center align-items-center mt-4">
+                <div class="col-md-5">
+                    <h5 class="text-dark text-capitalize creat">Men New-Season</h5>
+                    <h1>JACKETS & COATS</h1>
+                    <!-- <p>Lorem ipsum dolor sit, amet consectetur commodi ullam a tempora deserunt labore quam cumque
+                        magnam. Minus officiis voluptas voluptatum placeat animi! Sapiente in voluptates debitis.</p> -->
+                    <a class="btn btn-primary" href="intro.php">me</a>
+                    <!-- <button class="btn btn-white text-black log1 log">SIGNUP</button> -->
+                </div>
+
+
+                <div class="col-md-5 ">
+                    <img src="../php_class/images1/IMG_E1691.JPG" class="img-fluid" width="300" height="300" alt="">
+                </div>
+            </div>
+
+
+
+            
+        </div>
+
+
+         <div class="container my-5 ">
+        <div class="card-group ">
+              <?php for ($i=0; $i < count($folder); $i++) {?> 
+            <div class=" card col-md-4  text-black ms-2 border-0">
+                <div class="card1  d-flex justify-content-between align-items-center">
+                    <div class="ms-5 mb-5 fw-bold">
+                        
+                    <?= $folder[$i]["content1"] ?> 
+                    <br>
+                    <?= $folder[$i]["content2"]  ?> 
+                    </div>
+                      <img style="height: 100px; width: 100px;" src="photo/<?= $folder[$i]["image"]?>"  class="img-fluid img"  alt=""><span>
+                    </span>
+                </div>
+            </div>
+
+               <!-- <div class=" card col-md-4 bg-light text-black ms-2 border-0">
+                <div class="card1  d-flex justify-content-center align-items-center">
+                    <p class="me-5 mb-5"><b>Accessories</b> <br> New Trend</p>
+                    <img src="../php_class/images/product_image_1.jpg" class="my-5"
+                            height=" 150" alt=""><span>
+                    </span>
+                </div>
+            </div> -->
+              <!-- <div class=" card col-md-4 bg-light text-black ms-2 border-0">
+                <div class="card1  d-flex justify-content-center align-items-center">
+                    <p class="me-5 mb-5"><b>Accessories</b> <br> New Trend</p>
+                    <img src="../php_class/images/product_image_1.jpg" class="my-5"
+                            height=" 150" alt=""><span>
+                    </span>
+                </div>
+            </div> -->
+                <?php   }; ?>
+        </div>
+    </div>
+
+
+        <!-- product review -->
+    <div class="">
+        <div class="row me-5">
+            <div class="col-md-5 ms-1 mt-5">
+                <h1 class="fw-bold text-center ms-4" style="overflow: hidden; font-size: 30px;">PRODUCT OVERVIEW</h1>
+            </div>
+
+        </div>
+    </div>
+
+
+    <!-- product review navbar -->
+    <div class="navbar navbar-expand-md navbar-dark   ">
+        <div class="container justify-content-start d-flex align-items-center">
+            <ul class="navbar-nav ms-4">
+                <li class="navbar-item" style="text-decoration: underline;">
+                    <a href="" class="nav-link text-black text-uppercase text-center ms-5">all products</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="" class="nav-link text-black text-uppercase text-center ">women</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="" class="nav-link text-black text-uppercase text-center">men</a>
+                </li>
+                <li class="navbar-item">
+                    <a href="" class="nav-link text-black text-uppercase text-center">blog</a>
+                </li>
+                 <li class="navbar-item">
+                    <a href="" class="nav-link text-black text-uppercase text-center">shoes</a>
+                </li>
+                  <li class="navbar-item">
+                    <a href="" class="nav-link text-black text-uppercase text-center">watches</a>
+                </li>
+            </ul>
+
+
+        </div>
+
+        <div class="row justify-content-around align-items-center w-25 container me-5">
+            <button style="width: 100px;" class="btn btn-light mt-1 ms-5">filter</button>
+            <button style="width: 100px;" class="btn btn-light mt-2 ms-5">filter</button>
+
+        </div>
+        <!-- <div class="row justify-content-between d-flex align-items-center me-5">
+            <button style="width: 100px;" class="btn btn-primary">filter</button>
+
+        </div> -->
+    </div>
+    </div>
+
+        <div class="container my-5">
+        <div class="row flex wrap">
+            <?php for ($i=0; $i < count($document); $i++) {?> 
+                
+          
+            <div class="p-4 shadow col-md-3">
+                <!-- <img src="../php_class/images1/teacher.jpg" class="img-fluid img" height="600" width="400" alt=""> -->
+                <img src="photo/<?= $document[$i]["images1"]?>"  class="img-fluid img"  alt=""> <br>
+                <!-- <div><?= $document[$i]["header"] ?></div>
+                <br>
+                  <div><b><?= $document[$i]["title"] ?></b></div>
+                  <br>
+                  <div><?= $document[$i]["content"] ?></div>
+                  <br> -->
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div><?= $document[$i]["header"] ?></div>
+                  <div class="btn btn-dark"><i class="fa fa-heart" style="color: white; ;"></i></div>
+                    
+                  </div>
+            </div>
+
+            <?php   }; ?>
+        </div>
+    </div>
+
+            <!-- button after product review -->
+        <div class="d-flex justify-content-center  align-items-center text-black container mb-5">
+            <div class=" row">
+                <div class="col-md-12 text-center">
+                    <button style="width: 150px; background: gray;" class="btn text-white fw-bold  rounded-5 //">LOAD
+                        MORE</button>
+
+
+                </div>
+            </div>
+        </div>
+
+     <footer class="bg-black text-light">
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-md-4 mb-3">
+                    <h5 class="mb-3">About Us</h5>
+                    <p class="mb-0">
+                        we build mordern web solutions with speed, performance
+                    </p>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <h5 class="mb-3">Quick Links</h5>
+                    <ul class="list-unstyles">
+                        <li><a href="#" class="text-light text-decoration-none">Home</a></li>
+                        <li><a href="#" class="text-light text-decoration-none">Home</a></li>
+                         <li><a href="#" class="text-light text-decoration-none">Home</a></li>
+                        <li><a href="#" class="text-light text-decoration-none">Home</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 mb-3">
+                    <h5>Contact Us</h5>
+                    <ul class="list-unstyles">
+                        <li>Email: example@email.com</li>
+                        <li>Phone: +123 456 7890</li>
+                        <li>location: Lagos, Nigeria</li>
+                    </ul>
+                </div>
+            </div>
+
+            <hr class="border-secondary">
+
+                        <div class="text-center w-100" style="">
+                <small><i class="fa fa-instagram" style="background: red;"></i> <i class="fa fa-facebook" style="background: blue;"></i><i class="fa fa-whatsapp" style="background: greenyellow;"></i><i class="fa fa-telegram" style="background: blue;"></i><i class="fa fa-youtube" style="background: red;"></i></small>
+            </div>
+            <div class="text-center">
+                <small>@ 2025 your company, all rights reserved.</small>
+            </div>
+        </div>
+     </footer>
+    
+
+    
+</body>
+</html>
